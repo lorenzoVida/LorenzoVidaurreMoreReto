8de1d05965b041f36f3cb233983a9728b2cc8321
jloayza <lorenzo.vidaurre@complexless.com>
2024-08-29 13:22:47 -0500

    cambios en clases pages

--- pages/EmployePage.js
+++ pages/EmployePage.js
@@ -10,4 +10,16 @@ class EmployePage{
         await this.page.locator("//span[@class='oxd-text oxd-text--span oxd-main-menu-item--name'][normalize-space()='PIM']").click();
         await this.page.locator("//button[normalize-space()='Add']").click();
     }
+
+    async completeInformationNewEmploye(){
+        await this.page.getByPlaceholder("First Name").fill("Lorenzo");
+        await this.page.getByPlaceholder("Last Name").fill("Vidaurre");
+        await this.page.locator(".oxd-switch-input.oxd-switch-input--active.--label-right").click();
+        await this.page.locator("div:nth-of-type(3) > .orangehrm-full-width-grid.oxd-grid-2 > div:nth-of-type(1) > .oxd-input-field-bottom-space.oxd-input-group  .oxd-input").fill("LorenzoVidaureMore2024@");
+        await this.page.locator(".oxd-grid-item.oxd-grid-item--gutters.user-password-cell > .oxd-input-field-bottom-space.oxd-input-group  .oxd-input").fill("LorenzoVidaurreMore2024@");
+        await this.page.locator("div[class='oxd-grid-item oxd-grid-item--gutters'] div[class='oxd-input-group oxd-input-field-bottom-space'] div input[type='password']").fill("LorenzoVidaurreMore2024@");
+        await this. page.locator("button[type='submit']").click();
+       
+
+     }
 }
--- tests/orangeTest.spec.js
+++ tests/orangeTest.spec.js
@@ -8,26 +8,8 @@ test('login successful and add new employee', async ({ page }) => {
   const employe = new EmployePage(page);
   await login.gotToLoginPage();
   await login.login("Admin","admin123");
-
-
-  const tituloDashboard = await page.locator("//h6[normalize-space()='Dashboard']");
-  const tituloTexto = await page.locator("//h6[normalize-space()='Dashboard']").textContent();
-  await expect(tituloDashboard).toBeVisible
-  await expect(tituloDashboard).toHaveText('Dashboard')
-  console.log(tituloTexto)
-  employe.enterPIMMoodule();
-  
-
-  await page.getByPlaceholder("First Name").fill("Lorenzo");
-  await page.getByPlaceholder("Middle Name").fill("Lo");
-  await page.getByPlaceholder("Last Name").fill("Vidaurre");
-  await page.locator(".oxd-switch-input.oxd-switch-input--active.--label-right").click();
-  await page.locator("div:nth-of-type(3) > .orangehrm-full-width-grid.oxd-grid-2 > div:nth-of-type(1) > .oxd-input-field-bottom-space.oxd-input-group  .oxd-input").fill("LorenzoVidaureMore2024@");
-  await page.locator(".oxd-grid-item.oxd-grid-item--gutters.user-password-cell > .oxd-input-field-bottom-space.oxd-input-group  .oxd-input").fill("LorenzoVidaurreMore2024@");
-  await page.locator("div[class='oxd-grid-item oxd-grid-item--gutters'] div[class='oxd-input-group oxd-input-field-bottom-space'] div input[type='password']").fill("LorenzoVidaurreMore2024@");
-  await page.locator("button[type='submit']").click();
-  const btnPersonalDetail  = await page.locator("div:nth-of-type(1) > .orangehrm-tabs-item");
-  await expect(btnPersonalDetail).toBeVisible
+  await employe.enterPIMMoodule();
+  await employe.completeInformationNewEmploye();
   
 });
 
